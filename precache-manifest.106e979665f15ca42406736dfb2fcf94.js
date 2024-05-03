a9f4c834bb132236a40084c3b0a2a4436aa72983
kunal nayak <87607403+Kunal766@users.noreply.github.com>
2024-05-03 22:03:21 +0530

    Add files via upload
    intial

---
+++ precache-manifest.106e979665f15ca42406736dfb2fcf94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e63826c92d65c4ba26f52889113a3d96",
+    "url": "/newportfolio/index.html"
+  },
+  {
+    "revision": "fea8e42c4e6b08625895",
+    "url": "/newportfolio/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "afa89213f854be3aa8ab",
+    "url": "/newportfolio/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "fea8e42c4e6b08625895",
+    "url": "/newportfolio/static/js/2.ff7b95c9.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/newportfolio/static/js/2.ff7b95c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afa89213f854be3aa8ab",
+    "url": "/newportfolio/static/js/main.425baa77.chunk.js"
+  },
+  {
+    "revision": "6f8e42eaa6026192d43d",
+    "url": "/newportfolio/static/js/runtime-main.82f026ce.js"
+  }
+]);
